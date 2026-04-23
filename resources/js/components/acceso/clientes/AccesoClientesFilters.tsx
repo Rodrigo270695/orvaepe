7cdc9b98532ce8fc8dc02d8dac640cd1d911c43b
Rodrigo270695 <rodrigo_06_27@hotmail.com>
2026-04-23 00:46:26 -0500

    loin verify4

--- resources/js/components/acceso/clientes/AccesoClientesFilters.tsx
+++ resources/js/components/acceso/clientes/AccesoClientesFilters.tsx
@@ -16,7 +16,7 @@ export default function AccesoClientesFilters({
                 className ?? '',
             ].join(' ')}
         >
-            <div className="w-full min-w-0 max-w-md sm:w-auto">
+            <div className="w-full min-w-0 max-w-lg sm:w-auto">
                 <VentasSuscripcionesSearch
                     initialQuery={initialQuery}
                     wide
